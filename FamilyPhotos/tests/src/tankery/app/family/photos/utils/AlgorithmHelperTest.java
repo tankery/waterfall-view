ce8543713e92287f1f4f26da8b5a324bee9da16b
tankery <tankery.chen@gmail.com>
2012-10-15 23:25:10 +0800

    Add test project and documents for FamilyPhotos.

---
+++ FamilyPhotos/tests/src/tankery/app/family/photos/utils/AlgorithmHelperTest.java
@@ -0,0 +1,43 @@
+package tankery.app.family.photos.utils;
+
+import junit.framework.TestCase;
+
+public class AlgorithmHelperTest extends TestCase {
+
+    private final AlgorithmHelper.Comparetor<Integer> testComparetor = new AlgorithmHelper.Comparetor<Integer>() {
+
+        @Override
+        public int compareTarget(Integer obj1, Integer obj2) {
+            return obj1 - obj2;
+        }
+
+        @Override
+        public int itemRelatedToTarget(int index, Integer topLine) {
+            return index - topLine;
+        }
+    };
+
+    private static final int ITEM_LIST_LENGTH = 20;
+
+    private static final int[][] indexAndTarget = {
+        {0, ITEM_LIST_LENGTH, 0, ITEM_LIST_LENGTH},
+        {0, ITEM_LIST_LENGTH, 0, ITEM_LIST_LENGTH/2},
+        {0, ITEM_LIST_LENGTH, ITEM_LIST_LENGTH/2, ITEM_LIST_LENGTH}
+    };
+
+    public void testBinaryFindBetween() {
+        int count = indexAndTarget.length / 4;
+        for (int i = 0; i < count; i++) {
+            int[] params = indexAndTarget[i];
+            int[] result = AlgorithmHelper.binaryFindBetween(params[0],
+                                                             params[1],
+                                                             params[2],
+                                                             params[3],
+                                                             testComparetor);
+            int[] expect = { Math.max(params[0], params[2]),
+                            Math.min(params[1], params[3]) };
+            assertEquals(expect, result);
+        }
+    }
+
+}
